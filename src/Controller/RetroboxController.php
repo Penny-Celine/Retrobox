596954fabf8288f5a247ca466618aed713b0bdc4
Celine Maupoux <celine.maupoux@gmail.com>
2020-07-03 04:12:52 +0200

    add contact form (mailer not configured)

--- src/Controller/RetroboxController.php
+++ src/Controller/RetroboxController.php
@@ -4,6 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 use App\Entity\MiniConsole;
 
 class RetroboxController extends AbstractController
@@ -40,5 +46,63 @@ class RetroboxController extends AbstractController
             'miniConsole' => $miniConsole
         ]);
     }
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(Request $request, \Swift_Mailer $mailer)
+    {
+        $defaultData = ['message' => 'Entrez votre message...'];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'Entrez votre adresse mail'
+                ]
+            ])
+            ->add('message', TextareaType::class)
+            ->add('send', SubmitType::class, [
+                'label' => 'Envoyer'
+            ])
+            ->getForm();
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+
+            $message = (new \Swift_Message('Confirmation Email'))
+                    ->setFrom('retrobox.game@gmail.com')
+                    ->setTo('recipient@example.com')
+                    ->setBody(
+                        $this->renderView(
+                            // templates/contact.html.twig
+                            'confirm.html.twig',
+                            ['name' => $name]
+                        ),
+                        'text/html'
+                    );
+
+            $mailer->send($message);
+            
+            return $this->render(
+                            'contact.html.twig',
+                            ['formContact' => $form->createView(),
+                            'message' => 'Merci ! Votre message a bien été envoyé ! Vous allez recevoir un mail de confirmation']
+            );
+            
+        }
+        return $this->render('contact.html.twig', [
+            'formContact' => $form->createView()
+        ]);
+        
+    }
 }
+
+    
+
+
